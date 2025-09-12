850e9d97b658dbf831975cd72b58e392c2cbd0d1
NelTeano <jonelteano29@gmail.com>
2025-09-12 23:52:46 +0800

    create a form in checkout page and implement basic checkout receipt generate(need some improvements)

--- src/app/pos-pages/checkout/checkout.ts
+++ src/app/pos-pages/checkout/checkout.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 
 @Component({
   selector: 'app-checkout',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './checkout.scss'
 })
 export class Checkout {
+  constructor() {
+    pdfMake.vfs = pdfFonts.vfs;
+  }
 
+  generatePdf() {
+    const docDefinition: any = {
+      content: [
+        { text: 'BakeryApp Receipt', style: 'header' },
+        { text: 'Thank you for your purchase!', margin: [0, 20, 0, 8], style: 'normal' }
+        // Add more content as needed
+      ],
+      styles: {
+        header: { fontSize: 18, bold: true },
+        normal: { fontSize: 12 }
+      }
+    };
+    pdfMake.createPdf(docDefinition).open();
+  }
 }
